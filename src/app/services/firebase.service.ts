b6e981816ff656673ab2cd4c457cc1e913d6a822
seriouslag <Landon.Gavin@gmail.com>
2019-03-02 16:16:53 -0500

    updated service to use async await

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Blog } from '../models/Blog';
 import { Observable } from 'rxjs';
@@ -44,13 +44,14 @@ export class FirebaseService {
     return this.blogs;
   }
 
-  public refreshUser() {
-    if (this.auth.auth.currentUser) {
-      this.auth.auth.currentUser.reload().then(() => {
-        if (environment.production === false) {
-          console.log('refreshed user');
-        }
-      });
+  public async refreshUser() {
+    if (!this.auth.auth.currentUser) {
+      return;
+    }
+
+    await this.auth.auth.currentUser.reload();
+    if (!environment.production) {
+      console.log('refreshed user');
     }
   }
 
@@ -68,39 +69,74 @@ export class FirebaseService {
     const loginResponse: firebase.auth.UserCredential = await this.auth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
     const fireabaseUser: firebase.User = loginResponse.user;
 
-    const user: User = await this.db.object<User>('/users/' + fireabaseUser.uid).valueChanges().pipe(
-      take(1)
-    ).toPromise();
+    try {
+      const user: User = await this.db.object<User>('/users/' + fireabaseUser.uid).valueChanges().pipe(
+        take(1)
+      ).toPromise();
 
-    if (user != null) {
-      return;
+      if (user === null) {
+        console.log('Failed to login through Google provider.');
+        return;
+      }
+
+      const tempUser: User = {} as User;
+      tempUser.dateCreated = Date.now().toString();
+      tempUser.email = user.email || '';
+
+      if (user.uid) {
+        tempUser.uid = user.uid.toLowerCase();
+        tempUser.vanity = user.uid.toLowerCase();
+        this.setUserVanity(user.uid.toLowerCase());
+      }
+
+      if (fireabaseUser.displayName) {
+        const firstName = fireabaseUser.displayName.split(' ').slice(0, -1).join(' ');
+        const lastName = fireabaseUser.displayName.split(' ').slice(-1).join(' ');
+        tempUser.fname = firstName;
+        tempUser.lname = lastName;
+      }
+
+      this.saveUserToDB({
+        email: tempUser.email, fname: tempUser.fname, lname: tempUser.lname,
+        bio: '', job: '', company: '', twitter: '',
+        facebook: '', instagram: '', twitch: '', youtube: '',
+        google: '', uid: tempUser.uid, linkedin: '', resumeLink: '',
+        vanity: tempUser.uid,
+        dateCreated: tempUser.dateCreated, image: '', isVerified: false
+      } as User);
+    } catch (e) {
+      let message: string;
+      switch (e.code) {
+        case 'auth/account-exists-with-different-credential':
+          message = 'This account already exists with diffent credentials, please try signing in.';
+          break;
+        case 'auth/auth-domain-config-required':
+          message = 'System error. :(';
+          break;
+        case 'auth/cancelled-popup-request':
+          message = 'Sign in popup was canceled :(';
+          break;
+        case 'auth/operation-not-allowed':
+          message = 'This method of signing in is currently disabled, please try another method.';
+          break;
+        case 'auth/operation-not-supported-in-this-environment':
+          message = 'Must be https and running in registered domain.';
+          break;
+        case 'auth/popup-blocked':
+          message = 'Signin popup was blocked';
+          break;
+        case 'auth/popup-closed-by-user':
+          message = 'Sign in popup was canceled :(';
+          break;
+        case 'auth/unauthorized-domain':
+          message = 'This domain is not authorized for this signin method.';
+          break;
+        default:
+          message = 'Cannot process, unknown error';
+      }
+
+      this.snackBar.open(message, 'OK', { duration: 2000 });
     }
-
-    const tempUser: User = {} as User;
-    tempUser.dateCreated = Date.now().toString();
-    tempUser.email = user.email || '';
-
-    if (user.uid) {
-      tempUser.uid = user.uid.toLowerCase();
-      tempUser.vanity = user.uid.toLowerCase();
-      this.setUserVanity(user.uid.toLowerCase());
-    }
-
-    if (fireabaseUser.displayName) {
-      const firstName = fireabaseUser.displayName.split(' ').slice(0, -1).join(' ');
-      const lastName = fireabaseUser.displayName.split(' ').slice(-1).join(' ');
-      tempUser.fname = firstName;
-      tempUser.lname = lastName;
-    }
-
-    this.saveUserToDB({
-      email: tempUser.email, fname: tempUser.fname, lname: tempUser.lname,
-      bio: '', job: '', company: '', twitter: '',
-      facebook: '', instagram: '', twitch: '', youtube: '',
-      google: '', uid: tempUser.uid, linkedin: '', resumeLink: '',
-      vanity: tempUser.uid,
-      dateCreated: tempUser.dateCreated, image: '', isVerified: false
-    } as User);
   }
 
   public saveUserToDB(lgUser: User): Promise<void> {
@@ -151,7 +187,7 @@ export class FirebaseService {
     const uid = this.auth.auth.currentUser.uid;
     let user: User;
     let oldVanity: string;
-    return new Promise(resolve => {
+    return new Promise(() => {
       const req = this.getUserByUID(uid);
       req
       .pipe(
@@ -161,7 +197,7 @@ export class FirebaseService {
         oldVanity = user.vanity;
         this.db.object('/vanities').update({ [user.vanity]: null });
         this.db.object('users/' + uid).update({ vanity });
-        return this.db.object('/vanities').update({ [vanity]: user.uid });
+        this.db.object('/vanities').update({ [vanity]: user.uid });
       });
     });
   }
@@ -182,13 +218,15 @@ export class FirebaseService {
     return ret;
   }
 
-  public createUserFromEmail(email: string, password: string, fname: string, lname: string): Promise<firebase.User> {
-    return this.auth.auth.createUserWithEmailAndPassword(email, password).then(async (response) => {
-      if (!response) {
+  public async createUserFromEmail(email: string, password: string, fname: string, lname: string): Promise<firebase.User> {
+    try {
+      const createUserResponse = await this.auth.auth.createUserWithEmailAndPassword(email, password);
+
+      if (!createUserResponse) {
         console.log('The response from creating a user is', null);
       }
 
-      const user = response.user;
+      const user = createUserResponse.user;
 
       try {
         await this.saveUserToDB({
@@ -205,29 +243,36 @@ export class FirebaseService {
         this.snackBar.open('Failed to create your account please try again.', 'OK', { duration: 3000 });
       }
       return user;
-
-    }).catch((error: firebase.FirebaseError) => {
-      if (error.code === 'auth/weak-password') {
-        this.snackBar.open('Password is too weak', 'OK', { duration: 2000 });
-      } else if (error.code === 'auth/invalid-email') {
-        this.snackBar.open('Email is invalid', 'OK', { duration: 2000 });
-      } else if (error.code === 'auth/email-already-in-use') {
-        this.snackBar.open('This email is already in use', 'OK', { duration: 2000 });
-      } else if (error.code === 'auth/operation-not-allowed') {
-        this.snackBar.open('This is not allowed at this time', 'OK', { duration: 2000 });
-      } else {
-        this.snackBar.open('Cannot process, unknown error', 'OK', { duration: 2000 });
+    } catch (e) {
+      let message: string;
+      switch (e.code) {
+        case 'auth/weak-password':
+          message = 'Password is too weak';
+          break;
+        case 'auth/invalid-email':
+          message = 'Email is invalid';
+          break;
+        case 'auth/email-already-in-use':
+          message = 'This email is already in use';
+          break;
+        case 'auth/operation-not-allowed':
+          message = 'This is not allowed at this time';
+          break;
+        default:
+          message = 'Cannot process, unknown error';
       }
 
+      this.snackBar.open(message, 'OK', { duration: 2000 });
       return null;
-    });
+    }
   }
 
-  public loginWithEmailProvider(email: string, password: string): Promise<firebase.auth.UserCredential> {
-    return this.auth.auth.signInWithEmailAndPassword(email, password).then((response) => {
-      // Login successful
-      // Handle toast here?
-      const user = response.user;
+  public async loginWithEmailProvider(email: string, password: string): Promise<firebase.User> {
+    try {
+      const signInResponse = await this.auth.auth.signInWithEmailAndPassword(email, password);
+
+      const user = signInResponse.user;
+
       if (user.displayName) {
         this.snackBar.open('Logged in as ' + user.displayName, 'OK', { duration: 1750 });
       } else if (user.email) {
@@ -237,20 +282,32 @@ export class FirebaseService {
       }
 
       return user;
-    }).catch((error: any) => {
-      const errorCode = error.code;
-      this.snackBar.open(error.message, 'OK', {
+    } catch (e) {
+      const errorCode = e.code;
+      this.snackBar.open(e.message, 'OK', {
         duration: 2000,
       });
-      if (errorCode === 'auth/user-disabled') {
-      } else if (errorCode === 'auth/invalid-email') {
-      } else if (errorCode === 'auth/user-not-found') {
-      } else if (errorCode === 'auth/wrong-password') {
-      } else {
-        console.log('An unknown error occurred', error);
+
+      let message: string;
+      switch (errorCode) {
+        case 'auth/user-disabled':
+          message = 'This user is disabled.';
+          break;
+        case 'auth/invalid-email':
+          message = 'This email is invalid.';
+          break;
+        case 'auth/user-not-found':
+          message = 'User was not found.';
+          break;
+        case 'auth/wrong-password':
+          message = 'Incorrect password.';
+          break;
+        default:
+          message = 'An unknown error occurred';
       }
+      console.log('message', e);
 
       return null;
-    });
+    }
   }
 }
